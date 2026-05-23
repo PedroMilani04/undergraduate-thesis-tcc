681094fd1391932d83d3d3f3f59b8b41d95d1a47
PedroMilani04 <milanipedrovagula@gmail.com>
2026-05-23 19:30:12 -0300

    standardizing rfe folders

--- 4-modelos-generalistas/xgboost/xgboost-model-3-classesRFE.py
+++ 4-modelos-generalistas/xgboost/xgboost-model-3-classesRFE.py
@@ -184,7 +184,7 @@ def xgboostModel():
 
     # Definimos quantas features queremos manter (ex: as 15 melhores)
     # Se quiser que o modelo decida sozinho, use RFECV
-    n_features_to_select = 15
+    n_features_to_select = 3
     selector = RFE(estimator, n_features_to_select=n_features_to_select, step=1)
 
     print(f"   Executando RFE para selecionar as {n_features_to_select} melhores features...")
@@ -290,8 +290,8 @@ def xgboostModel():
              bbox=dict(boxstyle="round,pad=1", fc="#fff5f5", ec="red", alpha=0.9))
 
     plt.tight_layout()
-    plt.savefig('./4-modelos-generalistas/xgboost/eleicoes/ELEICOESRFE-matriz-baseline-3-classes-15.png', dpi=300)
-    print("\n[SUCESSO] Imagem salva como: 'ELEICOESRFE-matriz-baseline-3-classes-15.png'")
+    plt.savefig('./4-modelos-generalistas/xgboost/eleicoes/TAXAS+RFE-matriz-baseline-3-classes-3prova.png', dpi=300)
+    print("\n[SUCESSO] Imagem salva como: 'TAXAS+RFE-matriz-baseline-3-classes-3prova.png'")
 
     # --- 7. EXPORTAÇÃO DOS DADOS DE TREINO E TESTE ---
     print("\n--- 7. EXPORTANDO DADOS (TREINO/TESTE) ---")
